9b32e4658588f5adf8471a334626f31ed444016c
Jason A. Tran <jasonatran@gmail.com>
2018-09-27 14:10:47 -0700

    examples/ee250_lab07: added more comments and starter code

--- examples/ee250_lab07/main.c
+++ examples/ee250_lab07/main.c
@@ -12,38 +12,79 @@
  * @}
  */
 
+/* We've provided all the includes you need. Any functions you see should be 
+defined in these header files, so browse these header files and their 
+corresponding implementations (often the equivalent .c file) to learn more about
+the function calls. */
 #include <stdio.h>
 
 #include "msg.h"
+#include "xtimer.h"
+#include "udp_rx_thr.h"
+#include "net/ipv6/addr.h"
+#include "net/gnrc.h"
+#include "net/gnrc/netif.h"
  
+/* Print statements always slow code down. Use DEBUG() instead of printf() if
+you want to disable the print statements on the fly by setting ENABLE_DEBUG to 0 */
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
+/* Use macros to give a constant a name. Arbitrary numbers in code is usually
+ bad practice */
 #define MAIN_QUEUE_SIZE     (8)
+#define NUM_PKTS_TO_RX      (10)
+#define UDP_PORT            (8050)
 
 static msg_t main_msg_queue[MAIN_QUEUE_SIZE];
+
+struct 
+kernel_pid_t main_pid = NULL;
 
 int main(void)
 {
     msg_t msg;
-    msg_init_queue(main_msg_queue, MAIN_QUEUE_SIZE);
+    msg_init_queue(main_msg_queue, sizeof(main_msg_queue));
 
-    puts("Welcome to Lab 07!");
+    printf("Welcome to Lab 07!\n");
 
-    // kernel_pid_t udp_rx_thr_pid = ??
+    /* Code to simply print out the RIOT device's IPv6 address */
+    gnrc_netif_t *netif = NULL;
+    while ((netif = gnrc_netif_iter(netif))) {
+        ipv6_addr_t ipv6_addrs[GNRC_NETIF_IPV6_ADDRS_NUMOF];
+        int res = gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR, 0, ipv6_addrs,
+                                  sizeof(ipv6_addrs));
+
+        if (res < 0) {
+            continue;
+        }
+        for (unsigned i = 0; i < (unsigned)(res / sizeof(ipv6_addr_t)); i++) {
+            char ipv6_addr[IPV6_ADDR_MAX_STR_LEN];
+
+            ipv6_addr_to_str(ipv6_addr, &ipv6_addrs[i], IPV6_ADDR_MAX_STR_LEN);
+            printf("My address is %s\n", ipv6_addr);
+        }
+    }
+
+    /* Initialize udp_rx_thr with the custom struct type that defines the 
+    arguments in which the thread should use to run */
+    //udp_rx_args_t udp_rx_args = ?? //TODO
+    udp_rx_thr_init((void *) &udp_rx_args); 
     
     while(1)
     {
         msg_receive(&msg);
 
-        if (msg.content.value == 1) {
-            DEBUG("main: received shutdown signal from udp_rx_thr\n");
-            return 0;
+        if (msg.content.value == 0) {
+            printf("main: received shutdown signal from udp_rx_thr\n");
+            return 0; // when main exits, RIOT-OS shuts down
         }
 
+        DEBUG("main: received illegal message...\n")
         xtimer_usleep(1000000); //sleep just in case we rcv illegal msgs
     }
 
     /* should never be reached */
-    return -1; //ERROR!
+    DEBUG("ERROR!\n");
+    return -1; 
 }
--- examples/ee250_lab07/udp_rx_thr.c
+++ examples/ee250_lab07/udp_rx_thr.c
@@ -25,38 +25,81 @@
 #include "byteorder.h"
 #include "thread.h"
 #include "msg.h"
-#include "net/gnrc/pktdump.h"
 #include "net/gnrc.h"
-#include "net/icmpv6.h"
-#include "net/ipv6/addr.h"
-#include "net/ipv6/hdr.h"
-#include "net/tcp.h"
-#include "net/udp.h"
-#include "net/sixlowpan.h"
-#include "od.h"
+#include "udp_rx_thr.h"
 
-/* allocate a stack for the UDP receiver thread */
-static char udp_rx_thr_stack[THREAD_STACKSIZE_MAIN];
+static void print_rss(msg_t *msg)
+{
+    if (msg->type == GNRC_NETAPI_MSG_TYPE_RCV) {
 
-static void print_payload(gnrc_pktsnip_t *pkt)
+        gnrc_pktsnip_t *pkt = msg->content.ptr;
+        gnrc_pktsnip_t *snip = gnrc_pktsnip_search_type(pkt, GNRC_NETTYPE_NETIF);
+        gnrc_netif_hdr_t *hdr = snip->data;
+
+        /* *** TO-DO *** */
+
+        /* gnrc_netif_hdr_t has information about the signal quality */
+        /* The RSS value is equal to the RSSI from 'hdr' minus CC2538_RSSI_OFFSET */
+        /* You must cast the RSSI from 'hdr' to signed integer because it is a 2-complement number */
+        /* You have to print out the RSS of each packet received */
+        
+
+
+        /* Tell GNRC you are done with this packet so it can release the memory */
+        gnrc_pktbuf_release(pkt);
+    }
+}
+
+static void print_prr(uint32_t pkt_rcv, uint32_t num_pkts)
+{
+    /* *** TO-DO *** */
+
+    /* You know the number of packets you were supposed to receive */
+    /* and the number of packets you actually received. */
+    /* So, calculate what is the the Packet Reception Ratio and print out */
+
+}
+
+static void _print_payload(gnrc_pktsnip_t *pkt)
 {
     /* when the lower layers of RIOT's "gnrc" network stack receives a packet,
      it will deconstruct the bytes and give you the packet in a linked list of
      what they call "snips." Each "snip" is a struct of type gnrc_pktsnip_t. This
      struct gives you a pointer to data and the size of the data array in bytes.
-     This is NOT a null terminated string, so you can NOT use DEBUG() or printf().
-     Find the snip of type "NETTYPE_UNDEF" to get to the actual packet payload */ 
+     Do NOT assume the data is a null terminated string. The snip of type
+     NETTYPE_UNDEF is where the payload lies. The linked list is NOT circular.*/ 
 }
 
 static void *_udp_rx_thr(void *arg)
 {
-    unsigned int num_pkts = (unsigned int)arg;  
+    udp_rx_args_t *udp_rx_args = (udp_rx_args_t *)arg;  
+    kernel_pid_t main_pid = udp_rx_args->main_pid;
+    int num_pkts = udp_rx_args->num_pkts;
+    uint32_t udp_port = udp_rx_args->udp_port;
 
-    while (1) {
+    msg_t msg, reply; //TODO: why did we define `reply` for you? see gnrc.h.
+
+    //TODO: what is `msg_t reply` used for? see gnrc.h. This is a weird quirk of
+    //the RIOT-OS kernel design
+
+    int rcvd_all_pkts = 0;
+    int num_rcvd = 0;
+
+    /* TODO: intialize msg queue for async messages */
+
+    /* TODO: register this thread to UDP packets coming in on udp_port */
+
+    /* TODO: exit the while loop when num_pkts packets have been received */
+    while (!rcvd_all_pkts) {
         /* delete this comment after you address it! */
         /* todo: to recv msgs, what do you need to setup? */
         msg_receive(&msg);
 
+        /* use gnrc_pktdump.c as a model and refer to pkt.h for documentation */
+        /* if you receive a pointer to a packet, make sure to 
+        gnrc_pktbuf_release(msg.content.ptr) to free up space! This is not
+        accurately represented in the pkt.h documentation, but is called in 
+        _dump_snip() in gnrc_pktdump.c. */
         switch (msg.type) {
             //case ??:
             //case ??:
@@ -65,11 +108,31 @@ static void *_udp_rx_thr(void *arg)
         }
     }
 
-    /* never reached */
-    return NULL;
+    /* TODO: Unregister this thread from UDP packets. Technically unnecessary, 
+    but let's do it for completion and good practice. */
+
+    /* send shutdown message to main thread */
+    msg.type = NULL; //we are not using this field
+    //msg.content.value = ?? //TODO
+    msg_send(&msg, main_pid);
+
+    return 0;
 }
 
-kernel_pid_t udp_rx_thr_init(void *num_pkts)
+kernel_pid_t udp_rx_thr_init(void *args)
 {
-    //??
+    /* What is `args` supposed to be?!
+
+    C coders use a pointer to void to tell programmers that this pointer 
+    argument can point to anything you need. It could be a pointer to
+    a function, variable, struct, etc. This allows for code *flexibility*. 
+    When this is called in main(), we cast a pointer to udp_rx_args to (void *) 
+    because we know what this pointer actually points to since udp_rx_args_t is 
+    a type specific to udp_rx_thr.c/.h. That is, if you are calling 
+    udp_rx_thr_init(), you have already read this file which tells shows you
+    what will happen to the input argument `args`. */ 
+
+    /* use thread_create() here. You will need a statically allocated stack to
+    create a thread. Also, you should pass args to the thread function 
+    (i.e., _udp_rx_thr()) */
 }
