fcb05d94dfd90cecf52520c375ad24cb80be051d
Jason Tran <jasonatran@gmail.com>
2019-10-23 22:26:38 -0700

    lab09 skeleton code

--- examples/ee250_lab09/main.c
+++ examples/ee250_lab09/main.c
@@ -11,7 +11,7 @@
  * @{
  *
  * @file
- * @brief       example application for setting a periodic wakeup
+ * @brief       EE 250L Lab 09: RIOT-OS Warmup
  *
  * @author      Jason A. Tran <jasontra@usc.edu>
  *
@@ -22,16 +22,26 @@
 #include "thread.h"
 #include "xtimer.h"
 #include "timex.h"
+#include "random.h"
 
-#define INTERVAL (1U * US_PER_SEC)          /* 1 second */
-#define PRINT_INTERVAL (10U * US_PER_SEC)   /* 10 seconds */
+/* Print statements always slow down embedded systems. Also, sometimes you don't 
+   need all print statements unless you are debugging. Use DEBUG() instead of 
+   printf() if you want to disable the print statements on the fly by setting 
+   ENABLE_DEBUG to 0 */
+#define ENABLE_DEBUG (1)
+#include "debug.h"
 
-char producer_stack[THREAD_STACKSIZE_DEFAULT];
+#define PRNG_INITVAL        418 /* seed value for pseudorandom number generator */
+#define PROB_OF_1           0.3 /* probability of generating a "1" */
+#define NEW_VALUE_MSG_TYPE  0
+
+#define PRODUCER_INTERVAL   (1U * US_PER_SEC)   /* 1 second */
+#define PRINT_INTERVAL      (10U * US_PER_SEC)  /* 10 seconds */
 
 /* package constants into an enum type */
 typedef enum {
-    EVENT_1010 = 0,
-    EVENT_1001,
+    EVENT_1011 = 0,
+    EVENT_0001,
 } event_type_t; 
 
 typedef struct {
@@ -43,22 +53,45 @@ typedef struct {
 typedef struct {
     struct event_t *next;
     event_type_t type;
+    char pattern[4];
 } event_t;
 
-/* head of the linked list */
-event_t *head = NULL;
-
-mutex_t list_lock;
+char producer_stack[THREAD_STACKSIZE_DEFAULT]; 
+static event_t *head = NULL;    /* head of the linked list */
+static mutex_t list_lock;       /* mutex to prevent race conditions with the linked list */
 
 void *producer_thread(void *arg)
 {
     /* use input arg to extract the other thread process IDs */
     thread_pids_t *pids = (thread_pids_t *)arg; //cast it to the right type to parse
 
+    msg_t msg;
+    msg.type = NEW_VALUE_MSG_TYPE;
+    double num;
+
+    random_init(PRNG_INITVAL);
+
     while(1) {
-        xtimer_usleep(INTERVAL);
+        xtimer_usleep(PRODUCER_INTERVAL);
         printf("slept until...\n");
-        
+        num = random_real(); 
+        if (num < PROB_OF_1) {
+            /* a single ascii character is usually a byte, but we need to 
+            upgrade it to a 32 bit unsigned integer to conform to the struct */
+            msg.content.value = (uint32_t) '1';
+
+            /* send a message to both detector threads */ 
+            msg_send(&msg, pids->detectorA);
+            msg_send(&msg, pids->detectorB);
+        } else {
+            /* a single ascii character is usually a byte, but we need to 
+            upgrade it to a 32 bit unsigned integer to conform to the struct */
+            msg.content.value = (uint32_t) '0';
+
+            /* send a message to both detector threads */
+            msg_send(&msg, pids->detectorA);
+            msg_send(&msg, pids->detectorB);
+        }
     }
 
     /* this should never be reached */
@@ -68,37 +101,57 @@ void *producer_thread(void *arg)
 void *detectorA_thread(void *arg) {
     msg_t msg;
 
+    /* TODO: initialize a message queue of a size that is a power of 2 */
+
     while (1) {
         msg_receive(&msg)
 
         switch (msg.type) {
+            case NEW_VALUE_MSG_TYPE:
+                /* TODO: detect the pattern "1011" */
+                
+                /* TODO: if a full pattern is detected, add to the global*/
+            default:
+                DEBUG("dectectorA: unknown message type\n")
+                break;
 
         }
 
     }
 
+    /* this should never be reached */
+    return NULL;
 }
 
 void *detectorB_thread(void *arg) {
     msg_t msg;
 
+    /* TODO: initialize a message queue of a size that is a power of 2 */
+
     while (1) {
         msg_receive(&msg)
 
         switch (msg.type) {
-
+            case NEW_VALUE_MSG_TYPE:
+                /* TODO: detect the pattern "0001" */
+                
+                /* TODO: if a full pattern is detected, add */
+            default:
+                DEBUG("dectectorB: unknown message type\n")
+                break;
         }
     }
 
+    /* this should never be reached */
+    return NULL;
 }
 
 int main(void)
 {
-    msg_t msg;
+    thread_pids_t pids; /* pass to producer_thread to minimize globals and hard coded values */
 
-    thread_pids_t pids;
-
-    /* TODO: create detectorA_thread and detectorB_thread */
+    /* TODO: create detectorA_thread and detectorB_thread (make sure to setup 
+    their stacks) */
 
     //pids.detectorA = thread_create(???)
     
@@ -109,21 +162,19 @@ int main(void)
             THREAD_PRIORITY_MAIN - 1,               // set priority
             THREAD_CREATE_STACKTEST,                // for debugging
             producer_thread,                        // set thread function
-            (void *) &pids,                         // thread func. input arg 
+            /* TODO: pass pids by reference and type cast to conform to function prototype */
+            /* PASS_PIDS_HERE */,                   // thread func. input arg 
             "producer_thread");                     // thread name
-
 
     /* use main thread as the final printing thread */
     while (1) {
         xtimer_usleep(PRINT_INTERVAL);
 
         /* TODO: check if there are any events in the linked list and print the
-        sequences comma-separated followed by the total number printed in one
-        loop on a new line */
+        sequence strings comma-separated followed by the total number printed in 
+        one iteration of this loop on a new line. make sure to use a mutex! */
     }
 
-
-    thread_sleep();
-
+    /* this should never be reached */
     return 0;
 }
